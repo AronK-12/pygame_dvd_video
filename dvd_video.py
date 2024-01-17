b6f893ced7ff8872eaf128eeea4bbb5f8b07fe7f
Aronk-112 <aronkvaszta12@outlook.com>
2024-01-17 13:46:39 +0100

    Look at em bounce

--- dvd_video.py
+++ dvd_video.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os
 
 WIDTH:int = 1280
 HEIGHT:int = 720
@@ -10,16 +10,54 @@ def main():
     clock = pygame.time.Clock()
     running = True
 
+    deltaTime = 0
+
+    dvd_logos:list = []
+
+    dir_path:str = "assets"
+    for i in os.listdir(dir_path):
+        dvd_logos.append(pygame.image.load(f"./assets/{i}"))
+
+    current_logo = dvd_logos[0]
+
+    dvd_rect = current_logo.get_rect()
+    dvd_speed = [5, 5]
+
+    def change_logo(l):
+        logo = l
+        curr = dvd_logos.index(logo)
+        print(curr, logo)
+
+        if curr + 2 > len(dvd_logos):
+            logo = dvd_logos[0]
+        else:
+            logo = dvd_logos[curr + 1]
+
+        return logo
+
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill("blue")
+        screen.fill("black")
+
+        if dvd_rect.left < 0 or dvd_rect.right > WIDTH:
+            dvd_speed[0] = -dvd_speed[0]
+            current_logo = change_logo(current_logo)
+
+        if dvd_rect.top < 0 or dvd_rect.bottom > HEIGHT:
+            dvd_speed[1] = -dvd_speed[1]
+            current_logo = change_logo(current_logo)
+
+        screen.blit(current_logo, dvd_rect)
+
+        dvd_rect = dvd_rect.move(dvd_speed)
 
         pygame.display.flip()
 
-        clock.tick(60)
+        deltaTime = clock.tick(60) / 1000
 
     pygame.quit()
 
